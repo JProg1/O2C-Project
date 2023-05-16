d5f4f4b3d6bca9fe293bd779d51a0c296cc94320
JCottonD <131383936+JCottonD@users.noreply.github.com>
2023-05-16 12:16:19 +0100

    Router + Homepage

---
+++ project-code/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Container, Row, Col } from "react-bootstrap";
+// Homepage includes some react-bootstrap components to shape the page, introducing the bootstrap grid.
+export default function HomePage() {
+    return (
+        <div className="homePage">
+            <Container>
+
+
+            </Container>
+        </div>
+    );
+}
