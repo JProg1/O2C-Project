bc2f7c882771a23a1699b5e420464f79f4c8295e
Rachel Butcher <rachel.butcher@dwp.gov.uk>
2023-05-17 17:13:23 +0100

    Property details image url added and tested

--- project-code/src/components/PropertyCard.jsx
+++ project-code/src/components/PropertyCard.jsx
@@ -5,9 +5,11 @@ export default function PropertyCard({id, cust_ref, address, status, asking_pric
         <Card style={{marginBottom: 1 + 'em'}}>
         <Card.Body>
           <Card.Img variant="top" src={image}/>
-            <Card.Title>[{id}] {cust_ref}</Card.Title>
+            <Card.Title>Property: [{id}] <br/>
+            Customer: {cust_ref}</Card.Title>
             <Card.Text>
-                {address} ~ {status} ~ {asking_price}
+                {address} <br/>
+                 Currently: {status} ~ £{asking_price}.00
             </Card.Text>
         </Card.Body>
         </Card>
