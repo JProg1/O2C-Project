2a8f7f5a9eaaed4601f9e20e3fe8536bd4545707
Rachel Butcher <rachel.butcher@dwp.gov.uk>
2023-05-19 11:32:48 +0100

    On RB: property card updates

--- project-code/src/components/PropertyCard.jsx
+++ project-code/src/components/PropertyCard.jsx
@@ -1,6 +1,6 @@
 import { Card } from "react-bootstrap";
 // Will import from JSON as this item will be mapped
-export default function PropertyCard({id, cust_ref, address, status, asking_price, image }){
+export default function PropertyCard({id, cust_ref, address, status, asking_price, image, type, bedrooms, bathrooms, garden, garage }){
     return (
         <Card style={{width: '25rem', margin:'0 0 1rem 0'}}>
         <Card.Body>
@@ -9,7 +9,12 @@ export default function PropertyCard({id, cust_ref, address, status, asking_pric
             Customer: {cust_ref} </Card.Title>
             <Card.Text>
                 {address} <br/>
-                 Currently: <strong>{status}</strong> <br/ > £{asking_price}.00
+                Currently: <strong>{status}</strong> <br/> 
+                £{asking_price}.00 <br/>
+                Type: {type} <br/>
+                Beds: {bedrooms}; Baths: {bathrooms} <br/>
+                Garden: {garden} <br/>
+                Garage: {garage}
             </Card.Text>
         </Card.Body>
         </Card>
